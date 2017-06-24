245b9d883728c73be9908f2fea83698aef7fb0a3
Guillermo Maschwitz <guillermo@streamlinesocial.com>
2017-06-24 19:54:34 -0300

    [Tries:Contacts] segunda version con un par de modificaciones

--- tries_contacts_app.js
+++ tries_contacts_app.js
@@ -1,6 +1,9 @@
 ;(()=>{
     "use strict";
 
+    /**
+     * Trie "class" implementation
+     */
     function Trie(char) {
         var char = char || null;
         this.childs = {};
@@ -8,17 +11,20 @@
         this.endOfWord = false;
     };
 
+
     /**
      * Add a word to the trie
      * @param word
      */
     Trie.prototype.addWord = function(word) {
-        if(word.length === 1) {
+        if(!word.length){
+            return;
+        }else if(word.length === 1) {
             this.data = word;
             this.endOfWord = true;
         } else {
-            let firstChar = word[0];        // always a char
-            let nextChar = word[1];         // always a char
+            let firstChar = word[0];
+            let nextChar = word[1];
             let suffix = word.substr(1);
 
             if(this.data == null){
@@ -40,39 +46,51 @@
      * @param word
      */
     Trie.prototype.countWordsWithPrefix = function(word) {
+        word = word || '';
         let prefixCount = this.endOfWord ? 1 : 0;
         let suffix = word.substr(1);
-        if(word.length && word[0] != this.data){
-            return 0;
-        }else if(word.length > 1){
-            let nextChar = word[1];
-            if(typeof this.childs[nextChar] != 'undefined'){
-                return prefixCount + this.childs[nextChar].countWordsWithPrefix(suffix);
-            }else{
-                return prefixCount;
+
+        if(word.length) {
+            if(suffix.length) {
+                let nextChar = word[1];
+                if(typeof this.childs[nextChar] != 'undefined'){
+                    return prefixCount + this.childs[nextChar].countWordsWithPrefix(suffix);
+                }else{
+                    return prefixCount;
+                }
+            }else if(word[0] != this.data){
+                return 0;
             }
-        }else{
-            let childLetters = Object.getOwnPropertyNames(this.childs);
-            for(let i=0; i<childLetters.length; i++){
-                prefixCount += this.childs[childLetters[i]].countWordsWithPrefix(suffix);
-            }
-            return prefixCount;
         }
+
+        let childLetters = Object.getOwnPropertyNames(this.childs);
+        for(let i=0; i<childLetters.length; i++){
+            prefixCount += this.childs[childLetters[i]].countWordsWithPrefix(suffix);
+        }
+        return prefixCount;
     };
 
 
     var t = new Trie('');
+
+    //add hack
+    t.addWord('hack');
+    //add hackerrank
+    t.addWord('hackerrank');
+    //find hac
+    console.log(t.countWordsWithPrefix('hac'));
+    //find hak
+    console.log(t.countWordsWithPrefix('hak'));
     console.log(t.countWordsWithPrefix('f'));
-    /*
-        t.addWord('hack');
-        t.addWord('hacer');
-        t.addWord('haz');
-        t.addWord('hhhhh');
-        t.addWord('hack');
-        console.log(t.countWordsWithPrefix('a'));           // 0
-        console.log(t.countWordsWithPrefix('h'));           // 4
-        console.log(t.countWordsWithPrefix('hac'));         // 2
-        console.log(t.countWordsWithPrefix('ha'));          // 3
-        console.log(t.countWordsWithPrefix('hak'));         // 0
-        */
+    t.addWord('hack');
+    t.addWord('hacer');
+    t.addWord('haz');
+    t.addWord('hhhhh');
+    t.addWord('hack');
+    console.log(t.countWordsWithPrefix('a'));           // 0
+    console.log(t.countWordsWithPrefix('h'));           // 5
+    console.log(t.countWordsWithPrefix('hac'));         // 2
+    console.log(t.countWordsWithPrefix('ha'));          // 3
+    console.log(t.countWordsWithPrefix('hak'));         // 0
+
 })();
