e654512e265edcead61d2e7c9a5b8490c8cfab0a
Guillermo Maschwitz <guillermo@streamlinesocial.com>
2017-07-23 19:25:04 -0300

    [dp coin change] trying to avoid ( didnt work ) stack overflow by sharing values as prototype properties

--- dp_coin_change.js
+++ dp_coin_change.js
@@ -1,58 +1,73 @@
-let solve = (()=> {
+let Solver = (()=>{
     "use strict";
 
-    let sortNumbers = (a, b) => {
-        return b-a;
-    };
+    function Solver() {}
 
-    let appendOperand = (operands,coin) => {
-        let newOperandsString = operands + '-' + Number(coin).toString();
-        newOperandsString= newOperandsString.split('-');
-        newOperandsString = newOperandsString.map(Number);
-        newOperandsString = newOperandsString.sort(sortNumbers);
-        newOperandsString = newOperandsString.join('-');
-        return newOperandsString;
-    };
-
-    let recursiveSolution = (money, coins, operands, memo) => {
-        if(typeof memo[operands] == 'undefined') {
-            if(money === 0){
-                memo[operands] = 1;
-            }else if(money < 0){
-                memo[operands] = 0;
+    Solver.prototype.recursiveSolution = function(money, memoKey) {
+        if(typeof this.memo[memoKey] == 'undefined'){
+            if(money == 0) {
+                this.memo[memoKey] = 1;
             }else{
-                let ways = 0;
-                for(let i=0;i<coins.length;i++){
-                    let result = recursiveSolution(money-coins[i] , coins, appendOperand(operands,coins[i]), memo);
-                    if(result > 0){
-                        ways += result;
+                this.memo[memoKey] = 0;
+                for(let i=0; i<this.COINS_LEN;i++){
+                    if(money-this.COINS[i] >= 0){
+                        this.memo[memoKey] += this.recursiveSolution(money-this.COINS[i] , memoKey + Math.pow(10, i));
                     }
                 }
-
-                memo[operands] = ways;
             }
+            return this.memo[memoKey];
         }else{
-            if(money >= 0){
-                if(typeof memo[operands] != 'undefined'){
-                    return 0;
-                }
-            }
+            // we want to avoid counting twice
+            // two different diffs with the same
+            // operands in distinct order
+            // so we are only counting each unique set of operands once
+            return 0;
         }
-        return memo[operands];
     };
 
-    let solve = (money,coins) => {
-        // memorize the amount of solutions per diff
-        // the key is a string representation of the operations
-        // eg '4-2-1-1'
-        let memo = {};
-        let result = recursiveSolution(money,coins,Number(money).toString(),memo);
-        return result;
-    }
+    Solver.prototype.solve = function(money,coinsArr) {
 
-    return solve;
+        Object.defineProperty(this, 'money', {
+            value: money,
+            writable: false,
+            configurable: false,
+            enumerable: false
+        });
+
+        Object.defineProperty(this, 'COINS', {
+            value: coinsArr,
+            writable: false,
+            configurable: false,
+            enumerable: true
+        });
+
+        Object.defineProperty(this, 'COINS_LEN', {
+            value: this.COINS.length,
+            writable: false,
+            configurable: false,
+            enumerable: false
+        });
+
+        Object.defineProperty(this, 'memo', {
+            value: [],
+            writable: true,
+            configurable: false,
+            enumerable: false
+        });
+
+        return this.recursiveSolution(this.money,0);
+    };
+
+
+    return Solver;
 })();
 
 
-console.log(solve(10, '2 5 3 6'.split(' ').map(Number)));
+// console.log(solve(10, '2 5 3 6'.split(' ').map(Number))); // should be 5
+// console.assert(solve(10, '2 5 3 6'.split(' ').map(Number)) == 5);
+
+// console.log(solve(4, '1 2 3'.split(' ').map(Number))); // should be 4
+// console.assert(solve(4, '1 2 3'.split(' ').map(Number)) == 4);
+let s = new Solver();
+console.log(s.solve(92,'1 2 3 4 5 6 7 8 9 10 20'.split(' ').map(Number)));
 
